83567ea0d7e77d9807da8ba9db27d1ee95e9e856
AdonaiJehosua <adonaijehosua@gmail.com>
2024-08-12 21:32:18 +0500

    Добавит класс TSVOfferGenerator

---
+++ src/shared/const/const.ts
@@ -0,0 +1,3 @@
+export const HOTEL_IMAGE_PATH = 'https://13.design.htmlacademy.pro/static/hotel/';
+export const MAX_IMAGES_AMOUNT = 20;
+export const MAX_IMAGES_ARRAY_LENGTH = 6;
--- src/shared/halpers/common.ts
+++ src/shared/halpers/common.ts
@@ -1,13 +1,36 @@
-export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
+import {HOTEL_IMAGE_PATH, MAX_IMAGES_AMOUNT, MAX_IMAGES_ARRAY_LENGTH} from '../const/index.js';
+
+export function generateRandomValue(min: number, max: number, numAfterDigit = 0) {
   return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
 }
 
-export function getRandomItems<T>(items: T[]):T[] {
+export function getRandomItems<T>(items: T[]): T[] {
   const startPosition = generateRandomValue(0, items.length - 1);
   const endPosition = startPosition + generateRandomValue(startPosition, items.length);
   return items.slice(startPosition, endPosition);
 }
 
-export function getRandomItem<T>(items: T[]):T {
+export function getRandomItem<T>(items: T[]): T {
   return items[generateRandomValue(0, items.length - 1)];
+}
+
+export function generateRandomBoolean(): boolean {
+  return Math.random() > 0.5;
+}
+
+export function generatePreviewImagePath() {
+  const imageNumber = generateRandomValue(0, Math.floor(Math.random() * 4));
+  return `${HOTEL_IMAGE_PATH}${imageNumber}.jpg`;
+}
+
+export function generateImagesPaths() {
+  const previousValues: number[] = [];
+  for (let i = 1; i <= MAX_IMAGES_ARRAY_LENGTH; i++) {
+    let newValue = generateRandomValue(0, MAX_IMAGES_AMOUNT);
+    while (previousValues.includes(newValue)) {
+      newValue = generateRandomValue(0, MAX_IMAGES_AMOUNT);
+    }
+    previousValues.push(newValue);
+  }
+  return previousValues.map((value) => `${HOTEL_IMAGE_PATH}${value}.jpg`);
 }
