43c35b7f20d4faba1038e7d16dbf3dcfe80d86a6
AdonaiJehosua <adonaijehosua@gmail.com>
2024-09-01 19:33:50 +0500

    Добавит запись времени в модели пользователя и встроенную валидацию полей

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,14 +1,35 @@
 import {User} from '../../types/index.js';
 import {Document, model, Schema} from 'mongoose';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
-  name: String,
-  avatarUrl: String,
-  isPro: Boolean,
-  email: String,
-  token: String,
-});
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  avatarUrl: {
+    type: String,
+    required: true,
+  },
+  isPro: {
+    type: Boolean,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+}, {timestamps: true});
 
 export const UserModel = model<UserDocument>('User', userSchema);
