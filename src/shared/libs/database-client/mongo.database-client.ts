b7917c63820a40ce9ff553cce12a8e958eca3faa
AdonaiJehosua <adonaijehosua@gmail.com>
2024-09-01 16:21:15 +0500

    Реализует подключение к БД

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,8 +1,12 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 import { DatabaseClient } from './database-client.interface.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../logger/index.js';
+
+const RETRY_COUNT = 5;
+const RETRY_TIMEOUT = 1000;
 
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
@@ -26,10 +30,22 @@ export class MongoDatabaseClient implements DatabaseClient {
 
     this.logger.info('Trying to connect to MongoDB…');
 
+    let attempt = 0;
+    while (attempt < RETRY_COUNT) {
+      try {
+        this.logger.info('Database connection established.');
+        return;
+      } catch (error) {
+        attempt++;
+        this.logger.error(`Failed to connect to the database. Attempt ${attempt}`, error as Error);
+        await setTimeout(RETRY_TIMEOUT);
+      }
+    }
+
     this.mongoose = await Mongoose.connect(uri);
     this.isConnected = true;
 
-    this.logger.info('Database connection established.');
+    throw new Error(`Unable to establish database connection after ${RETRY_COUNT}`);
   }
 
   public async disconnect(): Promise<void> {
