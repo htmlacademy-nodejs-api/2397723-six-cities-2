044c3147c0534482e865e7018474efd62fbb8071
AdonaiJehosua <adonaijehosua@gmail.com>
2024-09-07 07:18:55 +0500

    Создаст сущность Offer

---
+++ src/shared/modules/offer/offer.entity.ts
@@ -0,0 +1,67 @@
+import {setLogLevel, defaultClasses, getModelForClass, modelOptions, prop, Ref} from '@typegoose/typegoose';
+import {City, Location, OfferGood, OfferType} from '../../types/index.js';
+import {UserEntity} from '../user/index.js';
+
+setLogLevel('debug');
+
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export interface OfferEntity extends defaultClasses.Base {
+}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'offers',
+    timestamps: true,
+  }
+})
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
+export class OfferEntity extends defaultClasses.TimeStamps {
+
+  @prop({required: true, unique: true, default: '', type: () => String})
+  public title: string;
+
+  @prop({required: true, type: () => OfferType})
+  public type: OfferType;
+
+  @prop({required: true, minlength: 100, maxlength: 100000, type: () => Number})
+  public price: number;
+
+  @prop({required: true})
+  public city: City;
+
+  @prop({required: true})
+  public location: Location;
+
+  @prop({required: true, type: () => Boolean})
+  public isFavorite: boolean;
+
+  @prop({required: true, type: () => Boolean})
+  public isPremium: boolean;
+
+  @prop({required: true, minlength: 1, maxlength: 5, type: () => Number})
+  public rating: number;
+
+  @prop({required: true, type: () => String})
+  public description: string;
+
+  @prop({required: true, type: () => String})
+  public images: string[];
+
+  @prop({required: true, type: () => String})
+  public previewImage: string;
+
+  @prop({required: true, type: () => OfferGood})
+  public goods: OfferGood[];
+
+  @prop({required: true, ref: () => UserEntity, type: () => String})
+  public hostId: Ref<UserEntity>;
+
+  @prop({required: true, type: () => Number})
+  public bedrooms: number;
+
+  @prop({required: true, type: () => Number})
+  public maxAdults: number;
+}
+
+export const OfferModel = getModelForClass(OfferEntity);
