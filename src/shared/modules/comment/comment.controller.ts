13d885dffff50058b472ca84de629adec7c0cc18
AdonaiJehosua <adonaijehosua@gmail.com>
2024-11-06 23:18:36 +0500

    Создаст мидлварю для проверки наличия документа в коллекции, подключит ее в нужных местах

--- src/shared/modules/comment/comment.controller.ts
+++ src/shared/modules/comment/comment.controller.ts
@@ -1,19 +1,26 @@
 import {inject, injectable} from 'inversify';
 import {Request, Response} from 'express';
-import {BaseController, HttpMethod, ValidateDtoMiddleware, ValidateObjectIdMiddleware} from '../../libs/rest/index.js';
+import {
+  BaseController,
+  DocumentExistsMiddleware,
+  HttpMethod,
+  ValidateDtoMiddleware,
+  ValidateObjectIdMiddleware
+} from '../../libs/rest/index.js';
 import {Logger} from '../../libs/logger/index.js';
 import {Component} from '../../types/index.js';
 import {CommentService} from './comment-service.interface.js';
 import {CommentRdo} from './rdo/comment.rdo.js';
 import {fillDTO} from '../../helpers/index.js';
 import {CreateCommentDto} from './dto/create-comment.dto.js';
-import {CreateOfferDto} from '../offer/index.js';
+import {CreateOfferDto, OfferServiceInterface} from '../offer/index.js';
 
 @injectable()
 export class CommentController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.CommentService) private readonly commentService: CommentService,
+    @inject(Component.OfferService) private readonly offerService: OfferServiceInterface,
   ) {
     super(logger);
 
@@ -23,13 +30,13 @@ export class CommentController extends BaseController {
       path: '/:offerId',
       method: HttpMethod.Get,
       handler: this.index,
-      middlewares: [new ValidateObjectIdMiddleware('offerId')]
+      middlewares: [new ValidateObjectIdMiddleware('offerId'), new DocumentExistsMiddleware(this.offerService, 'Offer', 'offerId'),]
     });
     this.addRoute({
       path: '/:offerId',
       method: HttpMethod.Post,
       handler: this.create,
-      middlewares: [new ValidateObjectIdMiddleware('offerId'), new ValidateDtoMiddleware(CreateOfferDto)]
+      middlewares: [new ValidateObjectIdMiddleware('offerId'), new ValidateDtoMiddleware(CreateOfferDto), new DocumentExistsMiddleware(this.offerService, 'Offer', 'offerId')]
     });
   }
 
