3045850751c69056714ce155e4f9a31135315413
AdonaiJehosua <adonaijehosua@gmail.com>
2024-11-21 21:00:45 +0500

    Добавит перенос строки

--- src/shared/modules/auth/auth.exception-filter.ts
+++ src/shared/modules/auth/auth.exception-filter.ts
@@ -1,9 +1,10 @@
-import { inject, injectable } from 'inversify';
-import { NextFunction, Request, Response } from 'express';
-import { ExceptionFilter } from '../../libs/rest/index.js';
-import { Component } from '../../types/index.js';
-import { Logger } from '../../libs/logger/index.js';
-import { BaseUserException } from './errors/index.js';
+import {inject, injectable} from 'inversify';
+import {NextFunction, Request, Response} from 'express';
+import {ExceptionFilter} from '../../libs/rest/index.js';
+import {Component} from '../../types/index.js';
+import {Logger} from '../../libs/logger/index.js';
+import {BaseUserException} from './errors/index.js';
+
 @injectable()
 export class AuthExceptionFilter implements ExceptionFilter {
   constructor(
@@ -11,8 +12,9 @@ export class AuthExceptionFilter implements ExceptionFilter {
   ) {
     this.logger.info('Register AuthExceptionFilter');
   }
+
   public catch(error: unknown, _req: Request, res: Response, next: NextFunction): void {
-    if (! (error instanceof BaseUserException)) {
+    if (!(error instanceof BaseUserException)) {
       return next(error);
     }
     this.logger.error(`[AuthModule] ${error.message}`, error);
