830d32a3389a47c2342dbbfd3cb1b794b3480372
AdonaiJehosua <adonaijehosua@gmail.com>
2024-11-24 19:50:30 +0500

    Разобьет обработчик ошибок на две сущности: обработчик ошибок http-запросов и обработчик ошибок валидации

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -22,6 +22,8 @@ export class RestApplication {
     @inject(Component.UserController) private readonly userController: Controller,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.AuthExceptionFilter) private readonly authExceptionFilter: ExceptionFilter,
+    @inject(Component.HttpExceptionFilter) private readonly httpExceptionFilter: ExceptionFilter,
+    @inject(Component.ValidationExceptionFilter) private readonly validationExceptionFilter: ExceptionFilter,
   ) {
     this.server = express();
     this.server.disable('x-powered-by');
@@ -63,6 +65,8 @@ export class RestApplication {
   private async _initExceptionFilters() {
     this.server.use(this.authExceptionFilter.catch.bind(this.authExceptionFilter));
     this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
+    this.server.use(this.validationExceptionFilter.catch.bind(this.validationExceptionFilter));
+    this.server.use(this.httpExceptionFilter.catch.bind(this.httpExceptionFilter));
   }
 
   public async init() {
--- src/shared/helpers/common.ts
+++ src/shared/helpers/common.ts
@@ -1,5 +1,9 @@
-import {HOST_AVATAR_PATH, HOTEL_IMAGE_PATH, MAX_IMAGES_AMOUNT, MAX_IMAGES_ARRAY_LENGTH} from '../const/index.js';
+import {ValidationError} from 'class-validator';
 import {ClassConstructor, plainToInstance} from 'class-transformer';
+
+import {HOST_AVATAR_PATH, HOTEL_IMAGE_PATH, MAX_IMAGES_AMOUNT, MAX_IMAGES_ARRAY_LENGTH} from '../const/index.js';
+import {ValidationErrorField} from '../types/validation-error-field.type.js';
+import {ApplicationError} from '../libs/rest/index.js';
 
 export function generateRandomValue(min: number, max: number, numAfterDigit = 0) {
   return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
@@ -23,6 +27,7 @@ export function generatePreviewImagePath() {
   const imageNumber = generateRandomValue(0, Math.floor(Math.random() * 4));
   return `${HOTEL_IMAGE_PATH}${imageNumber}.jpg`;
 }
+
 export function generateAvatarPath() {
   const imageNumber = generateRandomValue(0, Math.floor(Math.random() * 4));
   return `${HOST_AVATAR_PATH}${imageNumber}.jpg`;
@@ -45,11 +50,17 @@ export function getErrorMessage(error: unknown): string {
 }
 
 export function fillDTO<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
-  return plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
+  return plainToInstance(someDto, plainObject, {excludeExtraneousValues: true});
 }
 
-export function createErrorObject(message: string) {
-  return {
-    error: message,
-  };
+export function createErrorObject(errorType: ApplicationError, error: string, details: ValidationErrorField[] = []) {
+  return {errorType, error, details};
+}
+
+export function reduceValidationErrors(errors: ValidationError[]): ValidationErrorField[] {
+  return errors.map(({property, value, constraints}) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : []
+  }));
 }
